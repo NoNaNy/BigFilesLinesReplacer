1eab2460688e21d86358d9a9c0cfdf939632e25c
NoNaNy <pcldny@gmail.com>
2021-02-22 01:57:22 -0600

    Improvement performance sed search by line number

--- Sed.py
+++ Sed.py
@@ -5,12 +5,13 @@ rootPath=os.path.dirname(__file__)
 
 def SedLineNumber(lineNumber, fileName):
     print('--== SED ==--')
-    fullCommand=f"{rootPath}\\bin\\sed -n '{lineNumber},{lineNumber}p' {fileName}"
+    fullCommand=f"{rootPath}\\bin\\sed -n '{lineNumber},{lineNumber} {{p;{lineNumber}q}}' {fileName}"
     print(fullCommand)
     result = subprocess.run(fullCommand, stdout=subprocess.PIPE)
     textLine = result.stdout.decode('utf-8')
-    # print(textLine)
+    print(textLine)
     linesSed=textLine.splitlines()
+    print(f"Found: {len(linesSed)} lines")
     return linesSed
 
 def SedReplaceLine(numLine, strLine, fileName):
@@ -28,7 +29,9 @@ def SedReplaceLine(numLine, strLine, fileName):
 if __name__ == '__main__':
     pass
     # ret = SedLineNumber(4,'D:/Downloads/demo_sed.txt')    
-    # ret = SedLineNumber(7004515,'D:/Downloads/2020_241_Ordenes_20200828.txt')    
+    # ret = SedLineNumber(7004515,'D:/Downloads/2020_241_Ordenes_20200828.txt')
+    ret = SedLineNumber(515,'D:/Downloads/2020_241_Ordenes_20200828.txt')
+    # ret = SedLineNumber(5,'D:/Downloads/2020_241_Ordenes_20200828.txt')
     # dato="--==DATO REEMPLAZADO==--"
     # SedReplaceLine(21,dato,'D:\\Downloads\\demo_sed.txt')
     # dato="305;VIRTU USA LLC;0;0;XEXX010101000;0;8;3;0;840;E0150;403;419588;702;601;484;20200827;142510478;20200827;142510478;2203541814965;20200827;142510473;102;103;101;403;1;100;54.37;R;A;1;104;142526529;0;102;ROBERTO ARELLANO DE LA GARZA;20002;2537108;025019;1;309"
